262306bcdd58fbce9b7cfb75a157f34cef91a445
Mohamed Saleh <MohamedSaleh1984@hotmail.com>
2020-10-24 19:42:59 -0400

    added any_of

--- STL_CPP_11/STL_CPP_11.cpp
+++ STL_CPP_11/STL_CPP_11.cpp
@@ -1,18 +1,35 @@
-// STL_CPP_11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 using namespace std;
-
-
+void any_of();
+void all_of();
 
 int main()
 {
+	any_of();
+	all_of();	
+
+
 
 	return 0;
 }
 
+/*
+	any_of
+	------
+	Check if any element in the range satisfies the condition.
+*/
+void any_of()
+{
+	std::vector<int> v{ 5, 3, 7, 9, 4, 0 };
+	auto lambda = [](int elem) {
+		return elem == 0;
+	};
+
+	bool anyIsZero = std::any_of(v.begin(), v.end(), lambda);
+
+	cout << "Any of vector\'s elements is 0 :" << anyIsZero << endl;
+}
 
 /*
 	all_of
@@ -26,5 +43,5 @@ void all_of()
 		return i > 1;
 	};
 	bool allGreaterThanOne = std::all_of(v.begin(), v.end(), lambda);
-	std::cout << "All vector elements are greater than 1 " << allGreaterThanOne << endl;
+	std::cout << "All vector elements are greater than 1 :" << allGreaterThanOne << endl;
 }
